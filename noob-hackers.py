1c2fa263763a17be36d4b72d71794d035b7a7af3
Rizky598 <hakimrizky19@gmail.com>
2025-04-20 00:49:20 +0700

    Add files via upload

--- noob-hackers.py
+++ noob-hackers.py
@@ -1,70 +1,176 @@
-#!/bin/python
-import os
-import time
-import sys
-def load(word):
-	for char in word:
-		print(char,end='')
-		sys.stdout.flush()
-		time.sleep(0.2)
-def bash():
-	print("\033[31m")
-	print("██████╗ ███████╗██████╗ ██████╗     ███╗   ███╗██╗    ██╗ ██████╗ ██╗     ██╗   ██╗")
-	print("██╔══██╗██╔════╝██╔══██╗██╔══██╗    ████╗ ████║██║    ██║██╔═══██╗██║     ██║   ██║")
-	print("██████╔╝█████╗  ██████╔╝██║  ██║    ██╔████╔██║██║ █╗ ██║██║   ██║██║     ██║   ██║")
-	print("██╔══██╗██╔══╝  ██╔══██╗██║  ██║    ██║╚██╔╝██║██║███╗██║██║   ██║██║     ██║   ██║")
-	print("██████╔╝███████╗██║  ██║██████╔╝    ██║ ╚═╝ ██║╚███╔███╔╝╚██████╔╝███████╗╚██████╔╝")
-	print("╚═════╝ ╚══════╝╚═╝  ╚═╝╚═════╝     ╚═╝     ╚═╝ ╚══╝╚══╝  ╚═════╝ ╚══════╝ ╚═════╝\n\n")
-	time.sleep(3)
-	os.system("echo 'cmatrix -L' >> ~/.bashrc")
+#!/usr/python
 
 
-os.system("apt install cmatrix")
-os.system("echo 'cmatrix -L' >> ~/.bashrc")
+import os,sys,time
+os.system("clear")
+BB = "\033[34;1m" # Blue light
+YY = "\033[33;1m" # Yellow light
+GG = "\033[32;1m" # Green light
+WW = "\033[0;1m"  # White light
+RR = "\033[31;1m" # Red light
+CC = "\033[36;1m" # Cyan light
+B = "\033[34m"    # Blue
+Y = "\033[33m"    # Yellow
+G = "\033[32m"    # Green
+W = "\033[0m"     # White
+R = "\033[31m"    # Red
+C = "\033[36m"    # Cyan
+
+
 os.system("apt install mpv")
+os.system("mpv php.mp3")
 os.system("clear")
-print("Please wait, you can run this tool after this song..\n")
-time.sleep(4)
-print("Playing song..")
-os.system("mpv ssstik.io_1745081225462.mp3.mp3")
-os.system("clear")
-print("\033[33m")
-print("-----This is a private whatsapp hacking tool by *Mr b3rd%20mw01u* please use it educational purpose only-----")
-time.sleep(4)
-os.system('clear')
-print("\033[32;1m")
-print("██╗  ██╗ █████╗  ██████╗██╗  ██╗     ██╗    ██╗ █████╗")
-print("██║  ██║██╔══██╗██╔════╝██║ ██╔╝     ██║    ██║██╔══██╗")
-print("███████║███████║██║     █████╔╝█████╗██║ █╗ ██║███████║")
-print("██╔══██║██╔══██║██║     ██╔═██╗╚════╝██║███╗██║██╔══██║")
-print("██║  ██║██║  ██║╚██████╗██║  ██╗     ╚███╔███╔╝██║  ██║")
-print("╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝      ╚══╝╚══╝ ╚═╝  ╚═╝")
-time.sleep(3)
-print("\033[33m")
-print("[1] OTP bypass")
-print("[2] Whatsapp bombing")
-print("[3] Location Tracking")
-print("[4] Admin hijacking\n\n")
+def load(word):
+        for char in word:
+                print(char,end="")
+                sys.stdout.flush()
+                time.sleep(0.02)
+load(f"{GG} _   _             _           _                _\n")
+load(f"{RR}| \ | | ___   ___ | |__       | |__   __ _  ___| | _____ _ __ ___\n")
+load(f"{WW}|  \| |/ _ \ / _ \| '_ \ _____| '_ \ / _` |/ __| |/ / _ \ '__/ __|\n")
+load(f"{BB}| |\  | (_) | (_) | |_) |_____| | | | (_| | (__|   <  __/ |  \__\n")
+load(f"{CC}|_| \_|\___/ \___/|_.__/      |_| |_|\__,_|\___|_|\_\___|_|  |___/\n\n")
 
-u_choice=int(input('Choose a option:  '))
+
+print(f"{CC}Noob hackers all github tools available [https://github.com/noob-hackers]\n")
+print(f"{RR}Enter your choice\n\n")
+
+print(f"{BB}[1]  Vanish")
+print(f"{WW}🔰   All In One Virus Kit For Crashing Any Gadget In Seconds\n")
+
+print(f"{BB}[2]  Mrphish")
+print(f"{WW}🔰   All In One Social Accounts Phishing With Otp Bypass In Termux\n")
+
+print(f"{BB}[3]  Infect")
+print(f"🔰{WW}   Infect Any Android Device With Virus From Link In Termux\n\n")
+
+print(f"{BB}[4]  Snap")
+print(f"{WW}🔰   Bash Script For Backing Up All Files In Termux With Easy step\n")
+
+print(f"{BB}[5]  Autoig")
+print(f"{WW}🔰   Get More Stuff In Out Youtube Channel bitly.com/nhytchannel\n")
+
+print(f"{BB}[6]  P-gen")
+print(f"{WW}🔰   Generate Random And Strong Password In Termux With This Tool\n")
+
+print(f"{BB}[7]  T-Load")
+print(f"{WW}🔰   New Interface And Loading Screen For Termux Users\n")
+
+print(f"{BB}[8]  Hacklock")
+print(f"{WW}🔰   Hack Android Pattern From Termux With This Tool\n")
+
+print(f"{BB}[9]  IPdrone")
+print(f"{WW}🔰   Track Location With Live Address And Accuracy In Termux\n")
+
+print(f"{BB}[10] Lazybee")
+print(f"{WW}🔰   Create Best Wordlist From Python Tool In Termux\n")
+
+print(f"{BB}[11] Tunnel")
+print(f"{WW}🔰   Use Ngrok In Termux With Advanced Options\n")
+
+print(f"{BB}[12] Kalimux")
+print(f"{WW}🔰   Install And Use Kali Linux With Gui In Termux\n")
+
+print(f"{BB}[13] M-wiz")
+print(f"{WW}🔰   Install Metasploit And Repair Metasploit In Termux With Easy Steps\n")
+
+print(f"{BB}[14] Ighack")
+print(f"{WW}🔰   A tool to hack camera from termux for video tutorial visit youtube.com/noobhackers\n")
+
+print(f"{BB}[15] Grabcam")
+print(f"{WW}🔰   A tool to hack camera from termux for video tutorial visit youtube.com/noobhackers\n")
+
+u_choice=int(input(f"{CC}Choose a number you want >_ "))
+
 if u_choice==1:
+
      os.system("clear")
-     print("Processing please wait -")
-     time.sleep(1)
+     load(f"{RR}Please wait installing Vanish tool....[!]\n")
+     os.system("git clone https://github.com/noob-hackers/vanish")
+
+elif u_choice==2:
+
      os.system("clear")
-     print("Processing please wait /")
-     time.sleep(1)
+     load(f"{RR}Please wait installing Mrphish tool....[!]\n")
+     os.system("git clone https://github.com/noob-hackers/mrphish")
+
+elif u_choice==3:
+
      os.system("clear")
-     print("Processing please wait |")
-     time.sleep(1)
+     load(f"{RR}Please wait installing Infect tool....[!]\n")
+     os.system("git clone https://github.com/noob-hackers/infect")
+
+elif u_choice==4:
+
      os.system("clear")
-     print("Processing please wait \ ")
-     time.sleep(1)
+     load(f"{RR}Please wait installing Snap tool....[!]\n")
+     os.system("git clone https://github.com/noob-hackers/snap")
+
+elif u_choice==5:
+
      os.system("clear")
-     print("Processing please wait - ")
-     time.sleep(1)
+     load(f"{RR}Please wait installing Autoig....[!]\n")
+     os.system("git clone https://github.com/noob-hackers/autoig")
+
+elif u_choice==6:
+
      os.system("clear")
-     print("Processing please wait /")
-     time.sleep(1)
+     load(f"{RR}Please wait installing P-gen....[!]\n")
+     os.system("git clone https://github.com/noob-hackers/p-gen")
+
+elif u_choice==7:
+
      os.system("clear")
-     print("Process
+     load(f"{RR}Please wait installing T-load....[!]\n")
+     os.system("git clone https://github.com/noob-hackers/T-LOAD")
+
+elif u_choice==8:
+
+     os.system("clear")
+     load(f"{RR}Please wait installing Hacklock....[!]\n")
+     os.system("git clone https://github.com/noob-hackers/hacklock")
+
+elif u_choice==9:
+
+     os.system("clear")
+     load(f"{RR}Please wait installing IPdrone....[!]\n")
+     os.system("git clone https://github.com/noob-hackers/ipdrone")
+
+elif u_choice==10:
+
+     os.system("clear")
+     load(f"{RR}Please wait installing Lazybee....[!]\n")
+     os.system("git clone https://github.com/noob-hackers/lazybee")
+
+elif u_choice==11:
+
+     os.system("clear")
+     load(f"{RR}Please wait installing Tunnel....[!]\n")
+     os.system("git clone https://github.com/noob-hackers/tunnel")
+
+elif u_choice==12:
+
+     os.system("clear")
+     load(f"{RR}Please wait installing Kalimux....[!]\n")
+     os.system("git clone https://github.com/noob-hackers/kalimux")
+
+elif u_choice==13:
+
+     os.system("clear")
+     load(f"{RR}Please wait installing M-wiz....[!]\n")
+     os.system("git clone https://github.com/noob-hackers/m-wiz")
+
+elif u_choice==14:
+
+     os.system("clear")
+     load(f"{RR}Please wait installing Ighack....[!]\n")
+     os.system("git clone https://github.com/noob-hackers/ighack")
+
+elif u_choice==15:
+
+     os.system("clear")
+     load(f"{RR}Please wait installing Grabcam....[!]\n")
+     os.system("git clone https://github.com/noob-hackers/grabcam")
+else:
+
+     print("Sorry you have entered wrong choice !")
